86b9a4ef5d38494f70dd0e4c8262daac94c94519
Faisal Mohsen <rh7al@hotmail.com>
2023-03-11 20:40:50 +0300

    Update index.js

--- src/data/index.js
+++ src/data/index.js
@@ -1,28 +1,28 @@
-import beauty from './ar/beauty.json'
-import coffee from './ar/coffee.json'
-import friendship from './ar/friendship.json'
-import habits from './ar/habits.json'
-import happiness from './ar/happiness.json'
-import life from './ar/life.json'
-import loneliness from './ar/loneliness.json'
-import love from './ar/love.json'
-import marriage from './ar/marriage.json'
-import night from './ar/night.json'
-import risk from './ar/risk.json'
-import sadness from './ar/sadness.json'
+import الجمال from './ar/beauty.json'
+import القهوة from './ar/coffee.json'
+import الصداقة from './ar/friendship.json'
+import العادات from './ar/habits.json'
+import السعادة from './ar/happiness.json'
+import الحياة from './ar/life.json'
+import الوحدة from './ar/loneliness.json'
+import الحب from './ar/love.json'
+import الزواج from './ar/marriage.json'
+import الليل from './ar/night.json'
+import الحطر from './ar/risk.json'
+import الحزن from './ar/sadness.json'
 
 
 export {
-    beauty,
-    coffee,
-    friendship,
-    habits,
-    happiness,
-    life,
-    loneliness,
-    love,
-    marriage,
-    night,
-    risk,
-    sadness
+    الجمال,
+    الفهوة,
+    الصداقة,
+    العادات,
+    السعادة,
+    الحياة,
+    الوحدة,
+    الحب,
+    الزواج,
+    الليل,
+    الخطر,
+    الحزن
 }
